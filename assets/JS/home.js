6a449f16ac9eac302903f4ee25e53d03d9bdd001
LUUFER <imfa.cod@gmail.com>
2023-05-27 17:31:40 +0430

    creation the side nav cart and make it dynamic

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -3,24 +3,36 @@ let $ = document;
 let openLoginNav= $.querySelector('#open-login-nav');
 let closeLoginNav = $.querySelector('#close-login-nav');
 let sideNavLogin = $.querySelector('.sideNav__login');
+let openCartNav= $.querySelector('#open-cart-nav');
+let closeCartNav = $.querySelector('#close-cart-nav');
+let sideNavCart = $.querySelector('.sideNav__cart');
+
 let layer = $.querySelector('#layer');
 
-console.log(openLoginNav)
-console.log(closeLoginNav)
-console.log(sideNavLogin )
-
-// Open the login nav
+// Open/Close the login nav
 
 openLoginNav.addEventListener('click',openLoginNavHandeler);
 function openLoginNavHandeler (){
     sideNavLogin.classList.add("open__sideNav__login");
-    layer.classList.add("sideNav__login__layer--active");
+    layer.classList.add("sideNav__layer--active");
 };
-
-// Close the login nav
 
 closeLoginNav.addEventListener('click',closeLoginNavHandeler);
 function closeLoginNavHandeler (){
     sideNavLogin.classList.remove("open__sideNav__login");
-    layer.classList.remove("sideNav__login__layer--active");
+    layer.classList.remove("sideNav__layer--active");
+};
+
+// Open/Close the cart nav
+
+openCartNav.addEventListener('click',openCartNavHandeler);
+function openCartNavHandeler (){
+    sideNavCart.classList.add("open__sideNav__cart");
+    layer.classList.add("sideNav__layer--active");
+};
+
+closeCartNav.addEventListener('click',closeCartNavHandeler);
+function closeCartNavHandeler (){
+    sideNavCart.classList.remove("open__sideNav__cart");
+    layer.classList.remove("sideNav__layer--active");
 };
