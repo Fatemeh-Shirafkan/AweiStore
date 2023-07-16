2ac512f57dd3704d3d79319cfd1e88239cd4a540
LUUFER <imfa.cod@gmail.com>
2023-07-16 15:13:28 +0330

    complete the product page

--- assets/JS/product.js
+++ assets/JS/product.js
@@ -15,8 +15,25 @@ productImgBox.addEventListener("mouseleave", function(){
     productImg.style.transformOrigin = "center center";
 });
 
-
-
 addProductInterest.addEventListener("click", function(){
     addProductInterestIcon.classList.toggle("add__product__interest-active")
 });
+
+// TOGGLE TAB OF COMMENTS & DESCRIPTION
+
+let tabBtns = document.querySelectorAll(".tab__wrapper__list__item");
+let contents = document.querySelectorAll(".content__wrapper");
+let activeBtn = null;
+let activeContent = null;
+
+for(let i = 0 ;i < tabBtns.length; i++){
+
+    tabBtns[i].addEventListener("click", function(){
+        activeBtn = document.querySelector(".active.tab__wrapper__list__item");
+        activeContent = document.querySelector(".active.content__wrapper");
+        activeBtn.classList.remove("active");
+        activeContent.classList.remove("active");
+        tabBtns[i].classList.add("active");
+        contents[i].classList.add("active");
+    })
+};
