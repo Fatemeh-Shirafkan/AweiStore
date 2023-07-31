5da56215c33a30dcdce58026c0988865f636b9b5
LUUFER <imfa.cod@gmail.com>
2023-07-31 16:50:13 +0330

    set the dynamic braeadcrumb

--- assets/JS/product.js
+++ assets/JS/product.js
@@ -1,23 +1,214 @@
-let productImgBox = document.querySelector(".product__img");
-let productImg = document.querySelector(".product__img img");
-let addProductInterest = document.querySelector(".add__product__interest");
-let addProductInterestIcon = document.querySelector(".add__product__interest__icon")
+///////////////////////////////// PSEUDO DATABASE /////////////////////////////////
+
+let allProducts =[
+    {id: 1, 
+    title: 'T29P TWS Bluetooth Earphone', 
+    price: 1000000, 
+    img1:'./assets/images/products/headphone/earphone/T29PTWSBluetoothEarphone/T29PTWSBluetoothEarphone1.jpg', 
+    img2:'./assets/images/products/headphone/earphone/T29PTWSBluetoothEarphone/T29PTWSBluetoothEarphone2.jpg', 
+    img3:'./assets/images/products/headphone/earphone/T29PTWSBluetoothEarphone/T29PTWSBluetoothEarphone3.jpg', 
+    count: 1, 
+    address: 'T29P-TWS-Bluetooth-Earphone',
+    category: 'هدفون',
+    subCategory: 'ایرفون',
+    discount: 0,},
+
+    {id: 2, 
+    title: 'T36 TWS Stereo Earbuds', 
+    price: 1200000, 
+    img1:'./assets/images/products/headphone/earphone/T36TWSStereoEarbuds/T36TWSStereoEarbuds1.jpg', 
+    img2:'./assets/images/products/headphone/earphone/T36TWSStereoEarbuds/T36TWSStereoEarbuds2.jpg', 
+    img3:'./assets/images/products/headphone/earphone/T36TWSStereoEarbuds/T36TWSStereoEarbuds3.jpg', 
+    count: 1,
+    address: 'T36-TWS-Stereo-Earbuds',
+    category: 'هدفون',
+    subCategory: 'ایرفون',
+    discount: 0,},
+
+    {id: 3, 
+    title: 'T55 Bluetooth Earphone', 
+    price: 990000, 
+    img1:'./assets/images/products/headphone/earphone/T55BluetoothEarphone/T55BluetoothEarphone1.jpg', 
+    img2:'./assets/images/products/headphone/earphone/T55BluetoothEarphone/T55BluetoothEarphone2.jpg', 
+    img3:'./assets/images/products/headphone/earphone/T55BluetoothEarphone/T55BluetoothEarphone3.jpg', 
+    count: 1,
+    address: 'T55-Bluetooth-Earphone',
+    category: 'هدفون',
+    subCategory: 'ایرفون',
+    discount: 0,},
+
+    {id: 4, 
+    title: 'T55 Bluetooth Earphone', 
+    price: 1500000, 
+    img1:'./assets/images/products/headphone/earphone/TA3TWSANCEarbuds/TA3TWSANCEarbuds1.jpg', 
+    img2:'./assets/images/products/headphone/earphone/TA3TWSANCEarbuds/TA3TWSANCEarbuds2.jpg', 
+    count: 1,
+    address: 'TA3-TWS-ANC-Earbuds',
+    category: 'هدفون',
+    subCategory: 'ایرفون',
+    discount: 40,},
+
+    {id: 5, 
+    title: 'A710BL Bluetooth Headset', 
+    price: 9500000, 
+    img1:'./assets/images/products/headphone/gaming/A710BLBluetoothHeadset/A710BLBluetoothHeadset1.jpg', 
+    img2:'./assets/images/products/headphone/gaming/A710BLBluetoothHeadset/A710BLBluetoothHeadset2.jpg', 
+    img3:'./assets/images/products/headphone/gaming/A710BLBluetoothHeadset/A710BLBluetoothHeadset3.jpg', 
+    count: 1, 
+    address: 'A710BL-Bluetooth-Headset',
+    category: 'هدفون',
+    subCategory: 'گیمینگ',
+    discount: 20,},
+
+    {id: 6, 
+    title: 'ES-770i Bluetooth Headset', 
+    price: 2100000, 
+    img1:'./assets/images/products/headphone/gaming/ES-770iBluetoothHeadset/ES-770iBluetoothHeadset1.jpg', 
+    img2:'./assets/images/products/headphone/gaming/ES-770iBluetoothHeadset/ES-770iBluetoothHeadset2.jpg', 
+    count: 1, 
+    address: 'ES-770i-Bluetooth-Headset',
+    category: 'هدفون',
+    subCategory: 'گیمینگ',
+    discount: 0,},
+
+    {id: 7, 
+    title: 'Y280 Bluetooth Speaker', 
+    price: 1800000, 
+    img1:'./assets/images/products/speaker/portable/Y280BluetoothSpeaker/Y280BluetoothSpeaker1.jpg', 
+    img2:'./assets/images/products/speaker/portable/Y280BluetoothSpeaker/Y280BluetoothSpeaker2.jpg', 
+    img3:'./assets/images/products/speaker/portable/Y280BluetoothSpeaker/Y280BluetoothSpeaker3.jpg', 
+    count: 1,
+    address: 'Y28-Bluetooth-Speaker',
+    category: 'اسپیکر',
+    subCategory: 'قابل-حمل',
+    discount: 0,},
+
+    {id: 8, 
+    title: 'Y668 Bluetooth Speaker', 
+    price: 1700000, 
+    img1:'./assets/images/products/speaker/portable/Y668BluetoothSpeaker/Y668BluetoothSpeaker1.jpg', 
+    img2:'./assets/images/products/speaker/portable/Y668BluetoothSpeaker/Y668BluetoothSpeaker2.jpg', 
+    img3:'./assets/images/products/speaker/portable/Y668BluetoothSpeaker/Y668BluetoothSpeaker3.jpg', 
+    count: 1,
+    address: 'Y668-Bluetooth-Speaker',
+    category: 'اسپیکر',
+    subCategory: 'قابل-حمل',
+    discount: 22,},
+
+    {id: 9, 
+    title: 'Y669 Bluetooth Speaker', 
+    price: 2500000, 
+    img1:'./assets/images/products/speaker/portable/Y669BluetoothSpeaker/Y669BluetoothSpeaker1.jpg', 
+    img2:'./assets/images/products/speaker/portable/Y669BluetoothSpeaker/Y669BluetoothSpeaker2.jpg', 
+    img3:'./assets/images/products/speaker/portable/Y669BluetoothSpeaker/Y669BluetoothSpeaker3.jpg', 
+    count: 1,
+    address: 'Y669-Bluetooth-Speaker',
+    category: 'اسپیکر',
+    subCategory: 'قابل-حمل',
+    discount: 18,},
+
+    {id: 10, 
+    title: 'Y330 Bluetooth Speaker', 
+    price: 2500000, 
+    img1:'./assets/images/products/speaker/portable/Y330BluetoothSpeaker/Y330BluetoothSpeaker1.jpg', 
+    img2:'./assets/images/products/speaker/portable/Y330BluetoothSpeaker/Y330BluetoothSpeaker2.jpg', 
+    img3:'./assets/images/products/speaker/portable/Y330BluetoothSpeaker/Y330BluetoothSpeaker3.jpg', 
+    count: 1,
+    address: 'Y330-Bluetooth-Speaker',
+    category: 'اسپیکر',
+    subCategory: 'قابل-حمل',
+    discount: 0,},    
+
+    {id: 11, 
+    title: 'Y999 Bluetooth Speaker', 
+    price: 3200000, 
+    img1:'./assets/images/products/speaker/sandbar/Y999BluetoothSpeaker/Y999BluetoothSpeaker1.jpg', 
+    img2:'./assets/images/products/speaker/sandbar/Y999BluetoothSpeaker/Y999BluetoothSpeaker2.jpg', 
+    img3:'./assets/images/products/speaker/sandbar/Y999BluetoothSpeaker/Y999BluetoothSpeaker3.jpg', 
+    count: 1, 
+    address: 'Y999-Bluetooth-Speaker',
+    category: 'اسپیکر',
+    subCategory: 'ساندبار',
+    discount: 30,},
+];
+
+// GET URL LOCATION
+
+let locationSearch = location.search;
+let locationSearchParams = new URLSearchParams(locationSearch);
+let userIDParam = locationSearchParams.get('id');
+
+// FIND THE PRODUCT
+
+let targetProduct;
+
+targetProduct = allProducts.find(function(product){
+    return product.address == userIDParam
+});
+
+// CHANGE THE BRIDCRUMB OF PAGE ACCORDING TO THE ADDRESS
+
+let breadCrumbCategory = document.querySelector('#breadCrumb-category');
+let breadCrumbSubCategory = document.querySelector('#breadCrumb-subCategory');
+let breadCrumbTargetProduct = document.querySelector('#breadCrumb-targetProduct');
+
+breadCrumbCategory.innerHTML = "/ " + (targetProduct.category);
+breadCrumbSubCategory.innerHTML ="/ " + (targetProduct.subCategory).replace('-',' ');
+breadCrumbTargetProduct.innerHTML ="/ " + targetProduct.title;
+
+breadCrumbCategory.setAttribute("href","./store.html?id=" + targetProduct.category );
+breadCrumbSubCategory.setAttribute("href","./store.html?id=" + targetProduct.subCategory );
 
 // PRODUCT IMG HOVER FEATURE 
 
-productImgBox.addEventListener("mousemove", function(event){
-    productImg.style.transform = "scale(2)";
+let productImgBox = document.querySelector('.product__img');
+let productImg = document.querySelector('.product__img img');
+let addProductInterest = document.querySelector('.add__product__interest');
+let addProductInterestIcon = document.querySelector('.add__product__interest__icon');
+productImg.src = targetProduct.img1
+
+productImgBox.addEventListener('mousemove', function(event){
+    productImg.style.transform = 'scale(2)';
     productImg.style.transformOrigin = `${event.offsetX}px ${event.offsetY}px`;
 });
 
-productImgBox.addEventListener("mouseleave", function(){
+productImgBox.addEventListener('mouseleave', function(){
     productImg.style.transform = "scale(1)";
     productImg.style.transformOrigin = "center center";
 });
 
-addProductInterest.addEventListener("click", function(){
-    addProductInterestIcon.classList.toggle("add__product__interest-active")
+addProductInterest.addEventListener('click', function(){
+    addProductInterestIcon.classList.toggle('add__product__interest-active')
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // TOGGLE TAB OF COMMENTS & DESCRIPTION
 
