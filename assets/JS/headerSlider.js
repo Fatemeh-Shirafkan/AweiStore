397a9ec16cf58090aeb86c3093415fce3ad67dfc
FATEMEH <imfa.cod@gmail.com>
2024-03-02 21:13:22 +0330

    Set the confige of categories slider

--- assets/JS/headerSlider.js
+++ assets/JS/headerSlider.js
@@ -5,5 +5,40 @@ var swiper = new Swiper(".headerSlider", {
     grabCursor: 'true',
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper(".categoriesSlider", {
+    slidesPerView: 8,
+    spaceBetween: 5,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".categories-pagination",
+        clickable: true,
+        // dynamicBullets: true,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 5,
+        },
+        1024: {
+            slidesPerView: 7,
+        },
+        1140: {
+            slidesPerView: 8,
+        }
+
     },
 });
