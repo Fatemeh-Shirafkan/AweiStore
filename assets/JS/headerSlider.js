99d4606c4e3d5c6c1e2d0736e17b17b09ab07e2c
FATEMEH <imfa.cod@gmail.com>
2024-03-09 23:09:55 +0330

    Change the slider of product preview

--- assets/JS/headerSlider.js
+++ assets/JS/headerSlider.js
@@ -9,6 +9,8 @@ var swiper = new Swiper(".headerSlider", {
     },
 });
 
+//Categories Slider Styles
+
 var swiper = new Swiper(".categoriesSlider", {
     slidesPerView: 8,
     spaceBetween: 5,
@@ -40,3 +42,34 @@ var swiper = new Swiper(".categoriesSlider", {
 
     },
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 5,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+        1140: {
+            slidesPerView: 5,
+        }
+
+    },
+
+  });
